3753686b60f76b94597f423da50e9bc59d3d8ff6
Stas Solovev <oleluckoye@gmail.com>
2022-01-26 09:38:48 +0400

    update index.html, script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 "use strict";
-let nimber = 45;
-const leftBorderWidth = 1;
-number = 12;
-leftBorderWidth = 2;
+// let nimber = 45;
+// const leftBorderWidth = 1;
+// number = 12;
+// leftBorderWidth = 2;
 
-console.log(leftBorderWidth);
+// console.log(leftBorderWidth);
+
+// let result = confirm("are you ok?"); 
+// console.log(result);
+
+// const answer = prompt("are you ok?", "18");
+// console.log(answer);
+
+const answers = [];
+answers[0] = promt("Как тебя зовут?", "");
+answers[1] = promt("Как твоя фамилия?", "");
+answers[0] = promt("Сколько тебе лет?", "");
+
+console.log(answers);
